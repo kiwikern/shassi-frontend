1fe0c201f5da6f05a1ee22a56ddafb027b04472d
Kim Kern <github@kimkern.de>
2019-02-08 20:29:54 +0100

    fix unit test

--- src/app/products/product-filter/product-filter.component.spec.ts
+++ src/app/products/product-filter/product-filter.component.spec.ts
@@ -1,21 +1,48 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductFilterComponent } from './product-filter.component';
+import { Store } from '@ngrx/store';
+import {
+  MatButtonModule,
+  MatDividerModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatSelectModule,
+  MatSlideToggleModule
+} from '@angular/material';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { IAppState } from '../../reducers';
 
 describe('ProductFilterComponent', () => {
   let component: ProductFilterComponent;
   let fixture: ComponentFixture<ProductFilterComponent>;
+  let store: MockStore<IAppState>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductFilterComponent ]
+      declarations: [ProductFilterComponent],
+      imports: [
+        MatIconModule,
+        MatSelectModule,
+        MatFormFieldModule,
+        MatSlideToggleModule,
+        MatButtonModule,
+        MatDividerModule,
+      ],
+      providers: [
+        provideMockStore()
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductFilterComponent);
     component = fixture.componentInstance;
+    store = TestBed.get(Store);
+    // TODO: use overrideSelector instead when https://github.com/ngrx/platform/issues/1504
+    store.setState({products: {}});
+
     fixture.detectChanges();
   });
 
